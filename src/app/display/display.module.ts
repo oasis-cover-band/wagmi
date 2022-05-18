2d7afdace7af8be3b0b40a1dfdff68b05499b4e5
antonim <grebbenikov@tutanota.com>
2022-05-18 12:27:57 -0400

    initial structures

---
+++ src/app/display/display.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DisplayComponent } from './display.component';
+
+
+
+@NgModule({
+  declarations: [
+    DisplayComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class DisplayModule { }
